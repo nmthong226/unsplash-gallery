9ad41062e4cfd7fe5dcf1478574bbe6ddc36a29c
nmthong226 <nmthong226@gmail.com>
2024-10-27 21:59:54 +0700

    perf: enhance photo gallery

--- src/components/PhotoGallery/index.tsx
+++ src/components/PhotoGallery/index.tsx
@@ -1,80 +1,61 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Masonry from 'react-masonry-css';
-import './style.css';
+import PhotoDetail from '@/components/PhotoDetail';
 
-/**
- * PhotoGallery component
- *
- * @returns {JSX.Element} The component
- */
-const PhotoGallery = (): JSX.Element => {
-    const [photos, setPhotos] = useState<{ id: string; url: string }[]>([]);
-    const [page, setPage] = useState(1);
-    const [hasMore, setHasMore] = useState(true);
-    const [loading, setLoading] = useState(false); // New loading state
+interface PhotoGalleryProps {
+    photos: Photo[];
+    fetchPhotos: () => void;
+    hasMore: boolean;
+    loading: boolean;
+}
 
-    const fetchPhotos = async () => {
-        setLoading(true); // Start loading
-        try {
-            const response = await axios.get('https://api.unsplash.com/photos', {
-                params: {
-                    per_page: 10, // Limit to 10 photos per request
-                    page,
-                },
-                headers: {
-                    Authorization: `Client-ID ${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`,
-                },
-            });
+const PhotoGallery: React.FC<PhotoGalleryProps> = ({
+    photos,
+    fetchPhotos,
+    hasMore,
+    loading,
+}) => {
+    const [selectedPhoto, setSelectedPhoto] = useState<Photo | null>(null);
+    const [showModal, setShowModal] = useState(false);
 
-            const newPhotos = response.data.map((photo: any) => ({
-                id: photo.id,
-                url: photo.urls.small,
-            }));
+    const masonryBreakpoints = {
+        default: 3,
+        1100: 2,
+        700: 1,
+    };
 
-            setTimeout(() => {
-                setPhotos((prevPhotos) => {
-                    const uniquePhotos = newPhotos.filter(
-                        (newPhoto : any) => !prevPhotos.some((photo) => photo.id === newPhoto.id)
-                    );
-                    return [...prevPhotos, ...uniquePhotos];
-                });
-                setLoading(false);
-            }, 500);
+    const handlePhotoClick = (photo: Photo) => {
+        setSelectedPhoto(photo);
+        setShowModal(true);
+        window.history.pushState(null, '', `/photos/${photo.slug}`);
+    };
 
-            if (newPhotos.length < 10) {
-                setHasMore(false);
-            } else {
-                setPage((prevPage) => prevPage + 1);
-            }
-        } catch (error) {
-            console.error('Error fetching photos from Unsplash:', error);
-            setLoading(false);
-        }
+    const closeModal = () => {
+        setShowModal(false);
+        window.history.replaceState(null, '', '/photos');
     };
 
     useEffect(() => {
-        fetchPhotos();
+        const handlePopState = () => {
+            // Close modal if back button is clicked
+            setShowModal(false);
+        };
+
+        window.addEventListener('popstate', handlePopState);
+        return () => window.removeEventListener('popstate', handlePopState);
     }, []);
 
-    // Define breakpoints for the masonry layout
-    const masonryBreakpoints = {
-        default: 3, // 3 columns for large screens
-        1100: 2,    // 2 columns for medium screens
-        700: 1      // 1 column for small screens
-    };
-
     return (
-        <div className="flex flex-col w-full h-full items-center m-10">
+        <div className="flex flex-col w-full h-full items-center">
             <h1 className="text-3xl font-bold mb-4">Unsplash Gallery</h1>
             <InfiniteScroll
                 dataLength={photos.length}
                 next={fetchPhotos}
-                loader={''}
                 hasMore={hasMore}
+                loader={''}
                 endMessage={<p>No more photos to display.</p>}
-                className="w-full"
+                className="w-full m-10"
             >
                 <Masonry
                     breakpointCols={masonryBreakpoints}
@@ -82,22 +63,40 @@ const PhotoGallery = (): JSX.Element => {
                     columnClassName="masonry-grid_column"
                 >
                     {photos.map((photo) => (
-                        <img
+                        <div
                             key={photo.id}
-                            src={photo.url}
-                            alt={`Unsplash photo ${photo.id}`}
-                            className="rounded-md mb-4"
-                        />
+                            onClick={() => handlePhotoClick(photo)}
+                            className="relative group hover:cursor-pointer overflow-hidden rounded-md mb-4"
+                        >
+                            <img
+                                src={photo.urls.small}
+                                alt={`Unsplash photo ${photo.id}`}
+                                className="w-full h-full object-cover transform duration-300 group-hover:scale-110"
+                            />
+                            <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-md z-10">
+                                <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                    <div className="flex items-center absolute bottom-4 left-4 text-white space-x-2">
+                                        <img className="w-8 h-8 rounded-full" src={photo.author.avatar} alt={photo.author.username} />
+                                        <span className="text-sm md:text-base">{photo.author.username}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     ))}
                 </Masonry>
             </InfiniteScroll>
-            {loading ?
-                <div className='flex w-full justify-center mt-24'>
-                    <p className='text-center text-3xl font-bold'>Loading more photos...</p>
+            {loading && (
+                <div className="flex w-full justify-center items-center mt-24">
+                    <div className="w-16 h-16 border-[6px] border-dashed rounded-full border-t-blue-700 animate-spin"></div>
                 </div>
-                :
-                null
-            }
+            )}
+            {showModal && selectedPhoto && (
+                <PhotoDetail
+                    selectedPhoto={selectedPhoto}
+                    showModal={showModal}
+                    setShowModal={closeModal}
+                />
+            )}
         </div>
     );
 };
