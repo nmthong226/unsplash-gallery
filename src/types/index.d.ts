103b3d0c6102f9f0a313d89ce74b896621f15b1a
nmthong226 <nmthong226@gmail.com>
2024-10-27 22:01:14 +0700

    chore: add types file

---
+++ src/types/index.d.ts
@@ -0,0 +1,25 @@
+type Image_Urls = {
+    full: string;
+    raw: string;
+    regular: string;
+    small: string;
+    small_s3: string;
+    thumb: string;
+}
+
+type Author = {
+    username: string;
+    avatar: string;
+};
+
+type Photo = {
+    id: string;
+    urls: Image_Urls;
+    title: string;
+    author: Author;
+    description: string;
+    topic: string;
+    date: Date;
+    likes: string;
+    slug: string;
+};
