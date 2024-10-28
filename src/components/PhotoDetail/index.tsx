b552e069af2aee142775743b4e23e4a6a2e8711c
nmthong226 <nmthong226@gmail.com>
2024-10-28 11:53:35 +0700

    style: apply responsive

--- src/components/PhotoDetail/index.tsx
+++ src/components/PhotoDetail/index.tsx
@@ -23,13 +23,13 @@ const PhotoDetail: React.FC<PhotoDetailProps> = ({ selectedPhoto, showModal, set
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <button
-              className="absolute p-2 top-4 left-4 bg-black/20 hover:bg-black/30 rounded-full border-0 text-white opacity-100 text-3xl leading-none font-semibold outline-none focus:outline-none"
+              className="absolute p-2 top-4 left-4 bg-black/20 hover:bg-black/30 rounded-full border-0 text-white opacity-100 text-3xl leading-none font-semibold outline-none focus:outline-none z-10"
               onClick={() => setShowModal(false)}
             >
               <IoClose className='size-6' />
             </button>
-            <div className="relative mx-auto w-[1280px]">
-              <div className="border-0 rounded-lg shadow-lg flex flex-col w-full bg-white outline-none focus:outline-none relative">
+            <div className="relative mx-auto w-full sm:w-[640px] md:w-[768px] lg:w-[1280px] sm:px-2">
+              <div className="border-0 sm:rounded-lg shadow-lg flex flex-col w-full bg-white outline-none focus:outline-none relative">
                 <div className="flex items-center justify-between py-3 px-5 rounded-t">
                   <h3 className="flex">
                     <img src={selectedPhoto.author.avatar} alt={selectedPhoto.author.username} className='w-8 h-8 rounded-full mr-2' />
@@ -38,10 +38,10 @@ const PhotoDetail: React.FC<PhotoDetailProps> = ({ selectedPhoto, showModal, set
                   <DownloadButton imageUrl={selectedPhoto.urls.raw} sizes={downloadOptions} label="Download" />
                 </div>
                 <div className="flex items-center justify-center relative p-6 flex-auto">
-                  <img src={selectedPhoto.urls.full} alt={selectedPhoto.title} className="w-auto h-[540px] object-cover" />
+                  <img src={selectedPhoto.urls.full} alt={selectedPhoto.title} className="w-auto h-[320px] md:h-[400px] lg:h-[540px] object-cover" />
                 </div>
-                <div className="flex items-center justify-between p-6 rounded-b text-sm">
-                  <div className='flex space-x-20'>
+                <div className="flex flex-col sm:flex-row items-start sm:items-center sm:justify-between p-6 rounded-b text-sm">
+                  <div className='flex space-x-10 sm:space-x-6 md:space-x-12 lg:space-x-20 max-sm:mb-4'>
                     <div className='flex flex-col'>
                       <p className='text-gray-500'>Updated at</p>
                       <p>{selectedPhoto.date.toLocaleDateString()}</p>
@@ -55,10 +55,10 @@ const PhotoDetail: React.FC<PhotoDetailProps> = ({ selectedPhoto, showModal, set
                       <p>{selectedPhoto.topic ? selectedPhoto.topic : 'N/A'}</p>
                     </div>
                   </div>
-                  <div className='flex w-[500px] justify-end'>
+                  <div className='flex w-full sm:w-[300px] md:w-[400px] lg:w-[600px] justify-start sm:justify-end'>
                     <div className='flex flex-col'>
-                      <p className='flex text-gray-500 justify-end'>Description</p>
-                      <p className='w-full line-clamp-1'>{selectedPhoto.description}</p>
+                      <p className='flex text-gray-500 justify-start sm:justify-end'>Description</p>
+                      <p className='w-full line-clamp-2 sm:line-clamp-1 justify-start sm:justify-end'>{selectedPhoto.description}</p>
                     </div>
                   </div>
                 </div>
