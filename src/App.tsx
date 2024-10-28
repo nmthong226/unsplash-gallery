3bf6f768004845a8fa9c18ce200455aaf3d03851
nmthong226 <nmthong226@gmail.com>
2024-10-28 12:31:13 +0700

    style: apply error inform

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <Router>
-      <div className="flex w-full h-screen">
+      <div className="flex flex-col w-full h-screen">
         {error && <div className="error-message">Oops! {error}</div>}
         <Routes>
           <Route
